647a1b0acf4d8a702719b40f7fea72e53385d410
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-30 13:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  const logName = function(el, i, arr) {
+    console.log(el.name);
+  }
+
+  drivers.forEach(logName);
+}
+
+function logDriversByHometown(drivers, hometown) {
+  const logNameFilter = function(el, i, arr) {
+    if (el.hometown === hometown) {
+      console.log(el.name);
+    }
+  }
+  drivers.forEach(logNameFilter);
+}
